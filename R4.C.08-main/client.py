39d27743df1cb60ff081825cc9cc8c4f3d393b6b
feyy7435 <155959678+feyy7435@users.noreply.github.com>
2025-03-17 13:39:00 +0100

    Add files via upload

---
+++ R4.C.08-main/client.py
@@ -0,0 +1,81 @@
+import socket
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+class Client:
+    def __init__(self, port):
+        self.host = socket.gethostname()  # as both code is running on same pc
+        self.port = port
+        self.client_socket = socket.socket()  # instantiate
+        self.bfile = None
+
+    def connect(self):
+        self.client_socket.connect((self.host, self.port))  # connect to the server
+
+    def receiveFile(self):
+        # receive the size of the file
+        expected_size = b""
+        while len(expected_size) < 8:
+            more_size = self.client_socket.recv(8 - len(expected_size))
+            if not more_size:
+                raise Exception("Short file length received")
+            expected_size += more_size
+
+        # Convert to int, the expected file length
+        expected_size = int.from_bytes(expected_size, 'big')
+
+        # Until we've received the expected amount of data, keep receiving
+        self.bfile = b""  # Use bytes, not str, to accumulate
+        while len(self.bfile) < expected_size:
+            buffer = self.client_socket.recv(expected_size - len(self.bfile))
+            if not buffer:
+                raise Exception("Incomplete file received")
+            self.bfile += buffer
+        return self.bfile
+
+    def receiveMessage(self):
+        msg = self.client_socket.recv(1024).decode().split("@!")
+        return msg
+
+    def sendMessage(self, msg: str):
+        print("Sending:", msg)
+        self.client_socket.send(str.encode(msg + "@!"))
+
+    def saveFile(self, bytes: b"", filename: str):
+        with open(filename, 'wb') as f:
+            f.write(self.bfile)
+
+    def close(self):
+        if not self.client_socket == None:
+            self.client_socket.close()  # close the connection
+        else:
+            raise Exception("Erreur: la connexion a été fermée avant d'être instanciée.")
+
+
+    # def rsa_public_key():
+        private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+        pem_public_key = private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        public_key_file = open("output/public_key_rsa.txt", "w")
+        public_key_file.write(pem_public_key.decode())
+        public_key_file.close()
+
+
+
+if __name__ == '__main__':
+    
+    client = Client(5000)
+    client.connect()
+    bfile = client.receiveFile()
+    f = "output/filename.txt"
+    client.saveFile(bytes=bfile, filename=f)
+    msg = client.receiveMessage()
+    print(msg)
+    client.close()
+
+    
---
+++ R4.C.08-main/server.py
@@ -0,0 +1,79 @@
+import socket
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+class Server:
+    def __init__(self, port):
+        # get the hostname
+        host = socket.gethostname()
+        self.server_socket = socket.socket()  # get instance
+        # look closely. The bind() function takes tuple as argument
+        self.server_socket.bind((host, port))  # bind host address and port together
+        self.conn = None
+
+    
+    def waitForConnection(self):
+        # configure how many client the server can listen simultaneously
+        self.server_socket.listen(2)
+        self.conn, address = self.server_socket.accept()  # accept new connection
+        print("Connection from: " + str(address))
+
+    def sendMessage(self, msg: str):
+        print("Sending:", msg)
+        self.conn.send(str.encode(msg + "@!"))
+
+    def receiveMessage(self):
+        msg = self.conn.recv(1024).decode().split("@!")
+        return msg
+
+    def sendFile(self, filename: str):
+        print("Sending:", filename)
+        with open(filename, 'rb') as f:
+            raw = f.read()
+        # Send actual length ahead of data, with fixed byteorder and size
+        self.conn.sendall(len(raw).to_bytes(8, 'big'))
+        self.conn.send(raw)  # send data to the client
+
+
+    def close(self):
+        if not self.conn == None:
+            self.conn.close()  # close the connection
+        else:
+            raise Exception("Erreur: la connexion a été fermée avant d'être instanciée.")
+
+    def generate_private_key(self):
+        # Read the passphrase from the file input/test.txt
+        with open("input/test.txt", "r") as passfile:
+            private_key_pass = passfile.read().strip()  # Read and remove any extra whitespace
+
+        # Generate the private key using the passphrase
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+        encrypted_pem_private_key = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.BestAvailableEncryption(private_key_pass.encode())
+        )
+
+        # Save the private key to a file
+        private_key_file = open("input/private_key_rsa.txt", "w")
+        private_key_file.write(encrypted_pem_private_key.decode())
+        private_key_file.close()
+
+if __name__ == '__main__':
+    server = Server(5000)
+    server.waitForConnection()
+
+    # Generate and store the private key
+    server.generate_private_key()
+
+    f = "input/test.txt"
+    server.sendFile(filename=f)
+    server.sendMessage("Ce message a bien été transmis du serveur au client1")
+    server.sendMessage("Ce message a bien été transmis du serveur au client2")
+    server.sendMessage("Ce message a bien été transmis du serveur au client3")
+    server.sendMessage("Ce message a bien été transmis du serveur au client4")
+    server.sendMessage("Ce message a bien été transmis du serveur au client5")
+    server.close()
